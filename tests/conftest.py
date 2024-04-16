842d9ec9d57881941b9976264be70e3f11e76053
Daniil Bozhko <voc.offacc@gmail.com>
2024-04-16 10:58:36 +0000

    Fixed conftest and added failure test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,15 @@
 import pytest_asyncio
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import engine
 from app.models.base import Base
 
 
-@pytest_asyncio.fixture
-async def eng():
+@pytest_asyncio.fixture(scope='module', autouse=True)
+async def setup_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-        yield
+    yield
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
-@pytest_asyncio.fixture
-async def session(eng):
-    async with AsyncSession(eng) as session:
-        yield session
--- tests/test_repo.py
+++ tests/test_repo.py
@@ -1,27 +1,45 @@
 import pytest
-import pytest_asyncio
 from sqlalchemy import insert
+from sqlalchemy.exc import NoResultFound
 
-from app.db import async_session, engine
-from app.models.base import Base
+from app.db import async_session
 from app.models.student import Student
 from app.values.student import StudentDTO
 from app.repositories.students_repository import StudentsRepository
 
 
 @pytest.mark.asyncio
-async def test_succesful_find_by_chat_id(session):
-    students_query = insert(Student).values([{'student_guid': '720cc030-f2cc-4f31-8472-ee3a418270ac',
+async def test_succesful_find_by_chat_id():
+    async with async_session() as session:
+        students_query = insert(Student).values([{'student_guid': '720cc030-f2cc-4f31-8472-ee3a418270ac',
                                               'student_name': 'Dummy1', 'chat_id': 1},
                                              {'student_guid': '37e671c6-67b5-4077-83ed-93e621cd21d2',
                                               'student_name': 'Dummy2', 'chat_id': 2}
                                              ])
-    await session.execute(students_query)
-    repo = StudentsRepository(session)
+        await session.execute(students_query)
+        repo = StudentsRepository(session)
 
-    res = await repo.find_chatid(1)
+        res = await repo.find_chatid(1)
     
     assert res == StudentDTO(student_guid='720cc030-f2cc-4f31-8472-ee3a418270ac',
                              student_name='Dummy1',
                              chat_id=1)
+
+@pytest.mark.asyncio
+async def test_failed_find_by_chat_id():
+    async with async_session() as session:
+        students_query = insert(Student).values([{'student_guid': '719cc030-f2cc-4f31-8472-ee3a418270ac',
+                                              'student_name': 'Dummy0'},
+                                             {'student_guid': '36e671c6-67b5-4077-83ed-93e621cd21d2',
+                                              'student_name': 'Dummy1', 'chat_id': 2}
+                                             ])
+        await session.execute(students_query)
+        repo = StudentsRepository(session)
+        try:
+            res = await repo.find_chatid(1)
+            assert False
+        except NoResultFound:
+            assert True
+
+
 
